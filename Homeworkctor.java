17e1cddac7ae66847d329f19bcd6c52b6eaa701a
soozijan <75090137+soozijan@users.noreply.github.com>
2021-01-28 12:11:54 +0200

    Create Homeworkctor.java

---
+++ Homeworkctor.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+
+public class Country {
+    public String name;
+    public double size;
+    public int numberOfCitizens;
+    public String contient;
+
+
+   // public Country(String name, double size, int numberOfCitizens, String contient) {
+    //    this.name = name;
+     //   this.size = size;
+      //  this.numberOfCitizens = numberOfCitizens;
+      //  this.contient = contient;
+    }
+
+  //  @Override
+   // public String toString() {
+      //  return "Country{" +
+       //         "name='" + name + '\'' +
+        //        ", size=" + size +
+         //       ", numberOfCitizens=" + numberOfCitizens +
+         //       ", contient='" + contient + '\'' +
+         //       '}';
+  //  }
+//}
